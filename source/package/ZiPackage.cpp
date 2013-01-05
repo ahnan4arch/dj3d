216afc232c0a0d07c7fe0af229e56225a2eec7ee
Peter Xu <px.darkside@gmail.com>
2013-01-05 20:02:16 +0800

    add flex/bison & update zip

--- source/package/ZiPackage.cpp
+++ source/package/ZiPackage.cpp
@@ -4,450 +4,454 @@
 #include <assert.h>
 #include <time.h>
 
-static uLong GetDosDateTime(unsigned short mday, unsigned short month, unsigned short year, 
-                            unsigned short sec, unsigned short min, unsigned short hour)
+namespace dj3d
 {
-    uLong dosDateTime = 0;
-    *((unsigned short*)&dosDateTime + 1) = mday | (month + 1 << 5) | ((year - 80) << 9);
-    *((unsigned short*)&dosDateTime + 0) = (sec / 2) | (min << 5) | (hour << 11);
-    return dosDateTime;
-}
-
-class ZiPackageInternal
-{
-public:
-	explicit ZiPackageInternal(const std::string& strZipName);
-	bool CreateFile(const std::string& strFilePath);
-	bool WriteBulk(const MemoryBlock& blk);
-	bool FlushFile(void);
-	bool Close(void);
-	virtual ~ZiPackageInternal(void);
-private:
-	zipFile m_zf;
-	zip_fileinfo* m_zi;
-	zip_fileinfo  m_zi_info;
-}; // class ZiPackageInternal
-
-ZiPackageInternal::ZiPackageInternal(const std::string& strZipName):
-m_zf( zipOpen64( strZipName.c_str(), APPEND_STATUS_CREATE) ),
-m_zi( NULL )
-{
-	assert( NULL != m_zf );
-	return;
-}
-
-bool ZiPackageInternal::CreateFile(const std::string& strFilePath)
-{
-	if (NULL != m_zi)
-	{
-		return false;
-	}
-
-	m_zi = &m_zi_info;
-	memset( m_zi, 0, sizeof(m_zi) );
-    m_zi->external_fa = 32;
-
-    time_t now;
-    time(&now);
-    tm* local_time = localtime(&now);
-
-    m_zi->tmz_date.tm_sec = local_time->tm_sec;
-    m_zi->tmz_date.tm_min = local_time->tm_min;
-    m_zi->tmz_date.tm_hour = local_time->tm_hour;
-    m_zi->tmz_date.tm_mday = local_time->tm_mday;
-    m_zi->tmz_date.tm_mon = local_time->tm_mon;
-    m_zi->tmz_date.tm_year = local_time->tm_year;
-
-    // dos date
-    m_zi->dosDate = GetDosDateTime( local_time->tm_mday, 
-                                    local_time->tm_mon, 
-                                    local_time->tm_year, 
-                                    local_time->tm_sec, 
-                                    local_time->tm_min, 
-                                    local_time->tm_hour );
-    
-	return ZIP_OK == zipOpenNewFileInZip3_64( m_zf, strFilePath.c_str(), m_zi, 
-											  NULL, 0, NULL, 0, NULL, 
-											  Z_DEFLATED, Z_DEFAULT_COMPRESSION, 0,
-											  -MAX_WBITS, DEF_MEM_LEVEL, Z_DEFAULT_STRATEGY, 
-											  NULL, 0, 0 );
-}
-
-bool ZiPackageInternal::WriteBulk(const MemoryBlock& blk)
-{
-	if (NULL == m_zi)
-	{
-		return false;
-	}
-
-	if ( 0 != blk.size() )
-	{
-		return zipWriteInFileInZip( m_zf, blk.offset(0), static_cast<unsigned int>(blk.size()) ) >= 0;
-	}
-	else
-	{
-		return true;
-	}
-}
-
-bool ZiPackageInternal::FlushFile(void)
-{
-	if (NULL == m_zi)
-	{
-		return false;
-	}
-
-    bool bResult = (ZIP_OK == zipCloseFileInZip(m_zf));
-	m_zi = NULL;
-
-	return bResult;
-}
-
-bool ZiPackageInternal::Close(void)
-{
-	assert( NULL == m_zi );
-
-	if (NULL != m_zf)
-	{
-		bool bRtn = (ZIP_OK == zipClose(m_zf, NULL));
-		m_zf = NULL;
-		return bRtn;
-	}
-	else
-	{
-		return false;
-	}
-}
-	
-ZiPackageInternal::~ZiPackageInternal(void)
-{
-	Close();
-	return;
-}
-
-ZiPackage::ZiPackage(const std::string& strZipName):
-m_internal( new ZiPackageInternal(strZipName) )
-{
-	return;
-}
-
-bool ZiPackage::CreateFile(const std::string& strFilePath)
-{
-	if (NULL == m_internal)
-	{
-		return false;
-	}
-	else
-	{
-		return m_internal->CreateFile(strFilePath);
-	}
-}
-
-bool ZiPackage::WriteBulk(const MemoryBlock& blk)
-{
-	if (NULL == m_internal)
-	{
-		return false;
-	}
-	else
-	{
-		return m_internal->WriteBulk(blk);
-	}
-}
-
-bool ZiPackage::FlushFile(void)
-{
-	if (NULL == m_internal)
-	{
-		return false;
-	}
-	else
-	{
-		return m_internal->FlushFile();
-	}
-}
-
-bool ZiPackage::Close(void)
-{
-	if (NULL == m_internal)
-	{
-		return false;
-	}
-	else
-	{
-		return m_internal->Close();
-	}
-}
-
-ZiPackage::~ZiPackage(void)
-{
-	if (NULL != m_internal)
-	{
-		delete m_internal;
-		m_internal = NULL;
-	}
-
-	return;
-}
-
-class UnziPackageInternal
-{
-public:
-	UnziPackageInternal(const std::string& strZippedFileName);
-	bool NextFileInZip(std::string& strFileName);
-    bool FindFileInZip(const std::string& strFileName);
-	bool OpenFileInZip(void);
-	bool ReadBulk(MemoryBlock& blk, size_t& validLength);
-	bool ReadBulk(MemoryBlock& blk);
-	bool CloseFileInZip(void);
-	void Rewind(void);
-	virtual ~UnziPackageInternal(void);
-private:
-	ZPOS64_T m_cur;
-	unz_global_info64 m_gi;
-	unzFile m_uf;
-}; // class UnziPackageInternal
-
-UnziPackageInternal::UnziPackageInternal(const std::string& strZippedFileName):
-m_uf( unzOpen64(strZippedFileName.c_str() ) ),
-m_cur(0)
-{
-	memset(&m_gi, 0, sizeof(m_gi));
-
-	if (m_uf)
-	{
-		if (UNZ_OK != unzGetGlobalInfo64( m_uf, &m_gi) )
-		{
-			memset(&m_gi, 0, sizeof(m_gi));
-		}
-	}
-	return;
-}
-
-void UnziPackageInternal::Rewind(void)
-{
-	unzGoToFirstFile( m_uf );
-    m_cur = 0;
-    
-	return;
-}
-
-bool UnziPackageInternal::NextFileInZip(std::string& strFileName)
-{
-	strFileName.erase();
-
-	if (NULL == m_uf)
-	{
-		return false;
-	}
-
-	if (m_cur == m_gi.number_entry)
-	{
-		return false;
-	}
-
-	unz_file_info64 fileInfo;
-	char fileName[512];
-
-	if (m_cur ++ > 0)
-	{
-		if (UNZ_OK != unzGoToNextFile(m_uf))
-		{
-			return false;
-		}
-	}
-
-	if (UNZ_OK != unzGetCurrentFileInfo64( m_uf, &fileInfo, fileName, sizeof(fileName), NULL, 0, NULL, 0 ))
-	{
-		return false;
-	}
-
-	strFileName = fileName;
-
-	return true;
-}
-
-bool UnziPackageInternal::FindFileInZip(const std::string& strFileName)
-{
-    using namespace std;
-
-    if (0 == m_gi.number_entry)
+    static uLong GetDosDateTime(unsigned short mday, unsigned short month, unsigned short year, 
+        unsigned short sec, unsigned short min, unsigned short hour)
     {
-        return false;
+        uLong dosDateTime = 0;
+        *((unsigned short*)&dosDateTime + 1) = mday | ((month + 1) << 5) | ((year - 80) << 9);
+        *((unsigned short*)&dosDateTime + 0) = (sec / 2) | (min << 5) | (hour << 11);
+        return dosDateTime;
     }
 
-    if (m_cur == m_gi.number_entry)
+    class ZiPackageInternal
     {
-        Rewind();
+    public:
+        explicit ZiPackageInternal(const std::string& strZipName);
+        bool CreateFile(const std::string& strFilePath);
+        bool WriteBulk(const MemoryBlock& blk);
+        bool FlushFile(void);
+        bool Close(void);
+        virtual ~ZiPackageInternal(void);
+    private:
+        zipFile m_zf;
+        zip_fileinfo* m_zi;
+        zip_fileinfo  m_zi_info;
+    }; // class ZiPackageInternal
+
+    ZiPackageInternal::ZiPackageInternal(const std::string& strZipName):
+    m_zf( zipOpen64( strZipName.c_str(), APPEND_STATUS_CREATE) ),
+        m_zi( NULL )
+    {
+        assert( NULL != m_zf );
+        return;
     }
 
-    const ZPOS64_T savedPos = m_cur;
-    std::string strCandidate;
-    
-    do
+    bool ZiPackageInternal::CreateFile(const std::string& strFilePath)
     {
-        if (!NextFileInZip(strCandidate))
+        if (NULL != m_zi)
         {
-            break;
+            return false;
         }
-        if (0 == strFileName.compare(strCandidate))
+
+        m_zi = &m_zi_info;
+        memset( m_zi, 0, sizeof(m_zi) );
+        m_zi->external_fa = 32;
+
+        time_t now;
+        time(&now);
+        tm* local_time = localtime(&now);
+
+        m_zi->tmz_date.tm_sec = local_time->tm_sec;
+        m_zi->tmz_date.tm_min = local_time->tm_min;
+        m_zi->tmz_date.tm_hour = local_time->tm_hour;
+        m_zi->tmz_date.tm_mday = local_time->tm_mday;
+        m_zi->tmz_date.tm_mon = local_time->tm_mon;
+        m_zi->tmz_date.tm_year = local_time->tm_year;
+
+        // dos date
+        m_zi->dosDate = GetDosDateTime( local_time->tm_mday, 
+            local_time->tm_mon, 
+            local_time->tm_year, 
+            local_time->tm_sec, 
+            local_time->tm_min, 
+            local_time->tm_hour );
+
+        return ZIP_OK == zipOpenNewFileInZip3_64( m_zf, strFilePath.c_str(), m_zi, 
+            NULL, 0, NULL, 0, NULL, 
+            Z_DEFLATED, Z_DEFAULT_COMPRESSION, 0,
+            -MAX_WBITS, DEF_MEM_LEVEL, Z_DEFAULT_STRATEGY, 
+            NULL, 0, 0 );
+    }
+
+    bool ZiPackageInternal::WriteBulk(const MemoryBlock& blk)
+    {
+        if (NULL == m_zi)
+        {
+            return false;
+        }
+
+        if ( 0 != blk.size() )
+        {
+            return zipWriteInFileInZip( m_zf, blk.data(), static_cast<unsigned int>(blk.size()) ) >= 0;
+        }
+        else
         {
             return true;
         }
+    }
+
+    bool ZiPackageInternal::FlushFile(void)
+    {
+        if (NULL == m_zi)
+        {
+            return false;
+        }
+
+        bool bResult = (ZIP_OK == zipCloseFileInZip(m_zf));
+        m_zi = NULL;
+
+        return bResult;
+    }
+
+    bool ZiPackageInternal::Close(void)
+    {
+        assert( NULL == m_zi );
+
+        if (NULL != m_zf)
+        {
+            bool bRtn = (ZIP_OK == zipClose(m_zf, NULL));
+            m_zf = NULL;
+            return bRtn;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    ZiPackageInternal::~ZiPackageInternal(void)
+    {
+        Close();
+        return;
+    }
+
+    ZiPackage::ZiPackage(const std::string& strZipName):
+    m_internal( new ZiPackageInternal(strZipName) )
+    {
+        return;
+    }
+
+    bool ZiPackage::CreateFile(const std::string& strFilePath)
+    {
+        if (NULL == m_internal)
+        {
+            return false;
+        }
+        else
+        {
+            return m_internal->CreateFile(strFilePath);
+        }
+    }
+
+    bool ZiPackage::WriteBulk(const MemoryBlock& blk)
+    {
+        if (NULL == m_internal)
+        {
+            return false;
+        }
+        else
+        {
+            return m_internal->WriteBulk(blk);
+        }
+    }
+
+    bool ZiPackage::FlushFile(void)
+    {
+        if (NULL == m_internal)
+        {
+            return false;
+        }
+        else
+        {
+            return m_internal->FlushFile();
+        }
+    }
+
+    bool ZiPackage::Close(void)
+    {
+        if (NULL == m_internal)
+        {
+            return false;
+        }
+        else
+        {
+            return m_internal->Close();
+        }
+    }
+
+    ZiPackage::~ZiPackage(void)
+    {
+        if (NULL != m_internal)
+        {
+            delete m_internal;
+            m_internal = NULL;
+        }
+
+        return;
+    }
+
+    class UnziPackageInternal
+    {
+    public:
+        UnziPackageInternal(const std::string& strZippedFileName);
+        bool NextFileInZip(std::string& strFileName);
+        bool FindFileInZip(const std::string& strFileName);
+        bool OpenFileInZip(void);
+        bool ReadBulk(MemoryBlock& blk, size_t& validLength);
+        bool ReadBulk(MemoryBlock& blk);
+        bool CloseFileInZip(void);
+        void Rewind(void);
+        virtual ~UnziPackageInternal(void);
+    private:
+        ZPOS64_T m_cur;
+        unz_global_info64 m_gi;
+        unzFile m_uf;
+    }; // class UnziPackageInternal
+
+    UnziPackageInternal::UnziPackageInternal(const std::string& strZippedFileName):
+    m_uf( unzOpen64(strZippedFileName.c_str() ) ),
+        m_cur(0)
+    {
+        memset(&m_gi, 0, sizeof(m_gi));
+
+        if (m_uf)
+        {
+            if (UNZ_OK != unzGetGlobalInfo64( m_uf, &m_gi) )
+            {
+                memset(&m_gi, 0, sizeof(m_gi));
+            }
+        }
+        return;
+    }
+
+    void UnziPackageInternal::Rewind(void)
+    {
+        unzGoToFirstFile( m_uf );
+        m_cur = 0;
+
+        return;
+    }
+
+    bool UnziPackageInternal::NextFileInZip(std::string& strFileName)
+    {
+        strFileName.erase();
+
+        if (NULL == m_uf)
+        {
+            return false;
+        }
+
+        if (m_cur == m_gi.number_entry)
+        {
+            return false;
+        }
+
+        unz_file_info64 fileInfo;
+        char fileName[512];
+
+        if (m_cur ++ > 0)
+        {
+            if (UNZ_OK != unzGoToNextFile(m_uf))
+            {
+                return false;
+            }
+        }
+
+        if (UNZ_OK != unzGetCurrentFileInfo64( m_uf, &fileInfo, fileName, sizeof(fileName), NULL, 0, NULL, 0 ))
+        {
+            return false;
+        }
+
+        strFileName = fileName;
+
+        return true;
+    }
+
+    bool UnziPackageInternal::FindFileInZip(const std::string& strFileName)
+    {
+        using namespace std;
+
+        if (0 == m_gi.number_entry)
+        {
+            return false;
+        }
+
         if (m_cur == m_gi.number_entry)
         {
             Rewind();
         }
-    }while ( savedPos != m_cur );
 
-    return false;
-}
+        const ZPOS64_T savedPos = m_cur;
+        std::string strCandidate;
 
-bool UnziPackageInternal::OpenFileInZip(void)
-{
-	return UNZ_OK == unzOpenCurrentFile(m_uf);
-}
+        do
+        {
+            if (!NextFileInZip(strCandidate))
+            {
+                break;
+            }
+            if (0 == strFileName.compare(strCandidate))
+            {
+                return true;
+            }
+            if (m_cur == m_gi.number_entry)
+            {
+                Rewind();
+            }
+        }while ( savedPos != m_cur );
 
-bool UnziPackageInternal::ReadBulk(MemoryBlock& blk, size_t& validLength)
-{
-	int iRead = 0;
-	
-	iRead = unzReadCurrentFile( m_uf, blk.offset(0), static_cast<unsigned int>(blk.size()));
-	if (iRead < 0)
-	{
-		return false;
-	}
-	else
-	{
-		validLength = (size_t)iRead;
-		return UNZ_EOF != iRead;
-	}
-}
+        return false;
+    }
 
-bool UnziPackageInternal::ReadBulk(MemoryBlock& blk)
-{
-	unz_file_info64 fileInfo;
+    bool UnziPackageInternal::OpenFileInZip(void)
+    {
+        return UNZ_OK == unzOpenCurrentFile(m_uf);
+    }
 
-	if (UNZ_OK != unzGetCurrentFileInfo64( m_uf, &fileInfo, NULL, 0, NULL, 0, NULL, 0 ))
-	{
-		return false;
-	}
+    bool UnziPackageInternal::ReadBulk(MemoryBlock& blk, size_t& validLength)
+    {
+        int iRead = 0;
 
-	blk.alloc( fileInfo.uncompressed_size );
+        iRead = unzReadCurrentFile( m_uf, blk.data(), static_cast<unsigned int>(blk.size()));
+        if (iRead < 0)
+        {
+            return false;
+        }
+        else
+        {
+            validLength = (size_t)iRead;
+            return UNZ_EOF != iRead;
+        }
+    }
 
-	return unzReadCurrentFile( m_uf, blk.offset(0), static_cast<unsigned int>(blk.size())) == fileInfo.uncompressed_size;
-}
+    bool UnziPackageInternal::ReadBulk(MemoryBlock& blk)
+    {
+        unz_file_info64 fileInfo;
 
-bool UnziPackageInternal::CloseFileInZip(void)
-{
-	return UNZ_OK == unzCloseCurrentFile(m_uf);
-}
+        if (UNZ_OK != unzGetCurrentFileInfo64( m_uf, &fileInfo, NULL, 0, NULL, 0, NULL, 0 ))
+        {
+            return false;
+        }
 
-UnziPackageInternal::~UnziPackageInternal(void)
-{
-	if ( NULL != m_uf )
-	{
-		unzClose(m_uf);
-		m_uf = NULL;
-	}
+        blk.alloc( static_cast<size_t>(fileInfo.uncompressed_size) );
 
-	return;
-}
+        return unzReadCurrentFile( m_uf, blk.data(), static_cast<unsigned int>(blk.size())) == fileInfo.uncompressed_size;
+    }
 
-UnziPackage::UnziPackage(const std::string& strZippedFileName):
-m_internal( new UnziPackageInternal(strZippedFileName) )
-{
-	return;
-}
+    bool UnziPackageInternal::CloseFileInZip(void)
+    {
+        return UNZ_OK == unzCloseCurrentFile(m_uf);
+    }
 
-bool UnziPackage::NextFileInZip(std::string& strFileName)
-{
-	if (NULL == m_internal)
-	{
-		return false;
-	}
-	else
-	{
-		return m_internal->NextFileInZip(strFileName);
-	}
-}
+    UnziPackageInternal::~UnziPackageInternal(void)
+    {
+        if ( NULL != m_uf )
+        {
+            unzClose(m_uf);
+            m_uf = NULL;
+        }
 
-bool UnziPackage::FindFileInZip(const std::string& strFileName)
-{
-    if (NULL == m_internal)
-	{
-		return false;
-	}
-	else
-	{
-		return m_internal->FindFileInZip(strFileName);
-	}
-}
+        return;
+    }
 
-bool UnziPackage::OpenFileInZip(void)
-{
-	if (NULL == m_internal)
-	{
-		return false;
-	}
-	else
-	{
-		return m_internal->OpenFileInZip();
-	}
-}
+    UnziPackage::UnziPackage(const std::string& strZippedFileName):
+    m_internal( new UnziPackageInternal(strZippedFileName) )
+    {
+        return;
+    }
 
-bool UnziPackage::ReadBulk(MemoryBlock& blk, size_t& validLength)
-{
-	if (NULL == m_internal)
-	{
-		return false;
-	}
-	else
-	{
-		return m_internal->ReadBulk(blk, validLength);
-	}
-}
+    bool UnziPackage::NextFileInZip(std::string& strFileName)
+    {
+        if (NULL == m_internal)
+        {
+            return false;
+        }
+        else
+        {
+            return m_internal->NextFileInZip(strFileName);
+        }
+    }
 
-bool UnziPackage::ReadBulk(MemoryBlock& blk)
-{
-	if (NULL == m_internal)
-	{
-		return false;
-	}
-	else
-	{
-		return m_internal->ReadBulk(blk);
-	}
-}
+    bool UnziPackage::FindFileInZip(const std::string& strFileName)
+    {
+        if (NULL == m_internal)
+        {
+            return false;
+        }
+        else
+        {
+            return m_internal->FindFileInZip(strFileName);
+        }
+    }
 
-bool UnziPackage::CloseFileInZip(void)
-{
-	if (NULL == m_internal)
-	{
-		return false;
-	}
-	else
-	{
-		return m_internal->CloseFileInZip();
-	}
-}
+    bool UnziPackage::OpenFileInZip(void)
+    {
+        if (NULL == m_internal)
+        {
+            return false;
+        }
+        else
+        {
+            return m_internal->OpenFileInZip();
+        }
+    }
 
-void UnziPackage::Rewind(void)
-{
-	if (NULL != m_internal)
-	{
-		m_internal->Rewind();
-	}
+    bool UnziPackage::ReadBulk(MemoryBlock& blk, size_t& validLength)
+    {
+        if (NULL == m_internal)
+        {
+            return false;
+        }
+        else
+        {
+            return m_internal->ReadBulk(blk, validLength);
+        }
+    }
 
-	return;
-}
+    bool UnziPackage::ReadBulk(MemoryBlock& blk)
+    {
+        if (NULL == m_internal)
+        {
+            return false;
+        }
+        else
+        {
+            return m_internal->ReadBulk(blk);
+        }
+    }
 
-UnziPackage::~UnziPackage(void)
-{
-	if (NULL != m_internal)
-	{
-		delete m_internal;
-		m_internal = NULL;
-	}
-	return;
-}
+    bool UnziPackage::CloseFileInZip(void)
+    {
+        if (NULL == m_internal)
+        {
+            return false;
+        }
+        else
+        {
+            return m_internal->CloseFileInZip();
+        }
+    }
+
+    void UnziPackage::Rewind(void)
+    {
+        if (NULL != m_internal)
+        {
+            m_internal->Rewind();
+        }
+
+        return;
+    }
+
+    UnziPackage::~UnziPackage(void)
+    {
+        if (NULL != m_internal)
+        {
+            delete m_internal;
+            m_internal = NULL;
+        }
+        return;
+    }
+
+}; // namespace dj3d
